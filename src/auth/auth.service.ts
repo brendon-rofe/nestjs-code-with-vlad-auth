00babf4060b114ac2d9afacdd4d39639f9dd9538
Bravo1B9 <brofe93@gmail.com>
2023-05-23 10:00:39 +0000

    Some more auth setup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+
+  signupLocal() {};
+
+  signinLocal() {};
+
+  logout() {};
+
+  refreshTokens() {};
+
+}
