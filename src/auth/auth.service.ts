848b32b301bbd495a34eb975883e543692723c8c
Bravo1B9 <brofe93@gmail.com>
2023-05-23 10:15:23 +0000

    Initial signup setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private prismaService: PrismaService) {};
 
-  signupLocal() {};
+  async signupLocal(dto: AuthDto) {
+    const newUser = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        hash: dto.password
+      }
+    });
+  };
 
   signinLocal() {};
 
