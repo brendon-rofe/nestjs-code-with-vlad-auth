3e50977fc65315fc0387015176a3d3b744b696b9
Bravo1B9 <brofe93@gmail.com>
2023-06-14 14:50:17 +0000

    jwt setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule.register({
+    secret: 'super-secret'
+  })],
   providers: [AuthService],
   controllers: [AuthController]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-const users = require('../users.json');
+import * as users from '../users.json';
 import { AuthDto } from './dtos';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {};
 
   signinLocal(dto: AuthDto) {
     const user = users.find(user => user.email === dto.email);
     if(!user) throw new UnauthorizedException('Credentials incorrect');
     if(user.password !== dto.password) throw new UnauthorizedException('Credentials incorrect');
-    return user;
+    return { jwt: this.signUser(user.id, user.email, 'user') };
   };
 
   signupLocal(dto: AuthDto) {
 
+  };
+
+  signUser(userId: number, email: string, type: string) {
+    return this.jwtService.sign({
+      sub: userId,
+      email: email,
+      type: type
+    })
   };
 
 };
