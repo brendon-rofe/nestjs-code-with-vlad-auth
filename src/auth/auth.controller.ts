5dc20dfaa5b76d21abb8827f6c06c0caa412a82c
Bravo1B9 <brofe93@gmail.com>
2023-06-21 14:05:28 +0000

    Initial auth controller setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  
+  @Post('local/signup')
+  signupLocal() {
+    return { message: 'signup' }
+  };
+
+  @Post('local/signin')
+  signinLocal() {
+    return { message: 'signin' }
+  };
+
+  @Post('logout')
+  logout() {
+    return { message: 'logout' }
+  };
+
+  @Post('refresh')
+  refreshTokens() {
+    return { message: 'refresh' }
+  };
+};
