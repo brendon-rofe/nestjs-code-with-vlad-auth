f614a00483b9d41e3a50dbc51ab0dda439b4520a
Bravo1B9 <brofe93@gmail.com>
2023-06-21 14:35:37 +0000

    Update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dtos';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {};
 
-  signupLocal(dto: AuthDto) {
-    
+  hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+
+  async signupLocal(dto: AuthDto) {
+    const hash = await this.hashPassword(dto.password);
+
+    const newUser = this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hash
+      },
+    });
+    (await newUser).hash = undefined;
+    return newUser;
   };
 
   signinLocal() {
