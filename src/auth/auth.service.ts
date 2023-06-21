2a29a6ee609043c8c4e5e430cf1cf2fc3df383a4
Bravo1B9 <brofe93@gmail.com>
2023-06-21 14:27:01 +0000

    Update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dtos';
 
 @Injectable()
 export class AuthService {
-  signupLocal() {
-    return { message: 'signup' }
+  constructor(private prisma: PrismaService) {};
+
+  signupLocal(dto: AuthDto) {
+    
   };
 
   signinLocal() {
