b705638cb8b089a1bd411bc66b8616a93d864a3c
Bravo1B9 <brofe93@gmail.com>
2023-06-22 13:14:05 +0000

    Make certain endpoints public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,21 +2,21 @@ import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@n
 import { AuthService } from './auth.service';
 import { AuthDto } from './dtos';
 import { Tokens } from './types';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
 import { AtGuard, RtGuard } from 'src/common/guards';
-import {  GetCurrentUser, GetCurrentUserId } from 'src/common/decorators';
+import { GetCurrentUser, GetCurrentUserId, Public } from 'src/common/decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {};
   
+  @Public()
   @Post('local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signupLocal(dto);
   };
 
+  @Public()
   @Post('local/signin')
   @HttpCode(HttpStatus.OK)
   signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
